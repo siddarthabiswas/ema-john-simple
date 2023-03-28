bd45e35dc85a032183fe20d90e26488edfdf8e32
siddarthabiswas <sidubiswas6@gmail.com>
2023-03-28 12:29:41 +0600

    all the change added mow

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,18 @@ import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] =useState([]);
+    const [cart, setCart]=useState([])
+
     useEffect(()=>{
         fetch(`products.json`)
         .then(res => res.json())
         .then(data => setProducts(data))
-    },[])
+    },[]);
+
+    const handleAddToCart = (product)=>{
+        const newCart =[...cart,product];
+        setCart(newCart)
+    }
     return (
         <div className='shop-container'>
             <div className="product-container">
@@ -15,11 +22,13 @@ const Shop = () => {
                 products.map(product =><Product
                 key={product.id}
                 product ={product}
+                handleAddToCart={handleAddToCart}
                 ></Product>)
             }
             </div>
             <div className="cart-container">
             <h4>product summary</h4>
+            <p>select item:{cart.length}</p>
             </div>
         </div>
     );
