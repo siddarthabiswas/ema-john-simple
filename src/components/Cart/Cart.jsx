896251a2e12e42fe35b99f3c442c05b1d1864ddb
siddarthabiswas <sidubiswas6@gmail.com>
2023-03-29 16:49:58 +0600

    all function are working properly

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,20 +5,26 @@ const Cart = ({ cart }) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice = totalPrice + product.price;
+        // if (product.quantity = 0) {
+        //     product.quantity = 1
+        // }
+        // product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice * 7 / 100;
-    const grandTotal =totalPrice + totalShipping +tax;
+    const grandTotal = totalPrice + totalShipping + tax;
 
 
 
-    console.log(cart);
     return (
         <div className='cart'>
             <h4>product summary</h4>
-            <p>select item:{cart.length}</p>
+            <p>select item:{quantity}</p>
             <p>Total Price:{totalPrice}</p>
             <p>Total Shipping:${totalShipping}</p>
             <p>Tax:${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,35 +1,68 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
-    const [products, setProducts] =useState([]);
-    const [cart, setCart]=useState([])
-    
-    useEffect(()=>{
-        fetch(`products.json`)
-        .then(res => res.json())
-        .then(data => setProducts (data))
-    },[]);
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
 
-    const handleAddToCart = (product)=>{
-        const newCart =[...cart,product];
+    useEffect(() => {
+        fetch(`products.json`)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    useEffect(() => {
+        const storedCard = getShoppingCart();
+        const savedCart =[]
+        //step1: get id of the addedProduct
+        for (const id in storedCard) {
+            //get product from products state by id
+            const addedProduct = products.find(product =>product.id === id);
+            if(addedProduct){
+                //step:3 add quantity
+                const quantity =storedCard[id];
+                addedProduct.quantity =quantity;
+                // step 4: add  the added product to the save cart
+                savedCart.push(addedProduct)
+            }
+            
+        }
+        //step 5: set the cart
+        setCart(savedCart)
+    }, [products])
+
+    const handleAddToCart = (product) => {
+        let newCart =[];
+        // const newCart = [...cart, product];
+        const exists = cart.find(pd => pd.id===product.id);
+        if(!exists){
+            product.quantity =1;
+            newCart=[...cart,product]
+        }
+        else{
+            exists.quantity =exists.quantity + 1;
+            const remaining =cart.filter(pd => pd.id !== product.id);
+            newCart=[...remaining,exists];
+        }
         setCart(newCart)
+        addToDb(product.id)
     }
     return (
         <div className='shop-container'>
             <div className="product-container">
-            {
-                products.map(product =><Product
-                key={product.id}
-                product ={product}
-                handleAddToCart={handleAddToCart}
-                ></Product>)
-            }
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
-            <Cart cart={cart}></Cart>
-            
+                <Cart cart={cart}></Cart>
+
             </div>
         </div>
     );
